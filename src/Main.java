b1821c532611d2bb1510f64f9a8ab66796547bef
dmtevelavichius <tevelavichus@gmail.com>
2020-07-30 11:11:56 +0300

    Main.java new

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 400;
-        int newPayment = 1600;
-        boolean rightPayment = true;
-        if (rightPayment) {
-            int amountBonus = newPayment / 100;
-        } else  {
-            int amountBonus = 0;
+        int balance = 100;
+        int newPayment = 1000;
+        int amountBonus;
+        boolean bonus = true;
+        if (newPayment > 1000) {
+            amountBonus = newPayment / 100;
+        } else {
+            amountBonus = 0;
         }
-        int amountBonus = newPayment / 100;
-        long minLimit = newPayment;
-        if (newPayment > 1000 )
         System.out.println(amountBonus);
 
         long amount = balance + newPayment;
-                System.out.println(amount);
-                long totalBalance = amount + amountBonus;
+        System.out.println(amount);
+        long totalBalance = amount + amountBonus;
         System.out.println(totalBalance);
     }
 }
